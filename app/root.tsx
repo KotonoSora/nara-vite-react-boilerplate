a50a1121c1c94806a5ba803b26e6b3e44006c4c6
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2025-01-27 17:31:31 +0700

    style: 💄 Remove font Inter

--- app/root.tsx
+++ app/root.tsx
@@ -4,19 +4,7 @@ import type { Route } from './+types/root'
 
 import stylesheet from './app.css?url'
 
-export const links: Route.LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
-  { rel: 'stylesheet', href: stylesheet },
-]
+export const links: Route.LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
