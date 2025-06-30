89edd63212bd935e382451288a291d4276d3db3d
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2025-07-01 02:46:51 +0700

    feat: 🎸 implement links preconnect to google fonts

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,18 @@ import { themeSessionResolver } from "~/sessions.server";
 
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [];
+export const links: Route.LinksFunction = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+];
 
 export async function loader({ request }: Route.LoaderArgs) {
   const { getTheme } = await themeSessionResolver(request);
