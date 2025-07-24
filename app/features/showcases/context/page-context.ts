fcb16fa767ea1eb0f3c3b475f0e4e57e99c610b8
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2025-07-25 00:01:54 +0700

    feat: 🎸 enhance page context

--- app/features/showcases/context/page-context.ts
+++ app/features/showcases/context/page-context.ts
@@ -1,9 +1,13 @@
 import { createContext, useContext } from "react";
 
-export const PageContext = createContext<{
-  showcases: ProjectInfo[];
-}>({
+export const PageContext = createContext<PageInformation>({
+  title: "",
+  description: "",
+  githubRepository: "",
+  commercialLink: "",
   showcases: [],
+  steps: [],
+  featuresConfig: [],
 });
 
 export const usePageContext = () => useContext(PageContext);
