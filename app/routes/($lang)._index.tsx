ff517f3ba4f1bb06219c8a07484f8e197dcd1a3e
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2025-09-17 10:56:14 +0700

    feat: 🎸 remove double check type after apply middleware

--- app/routes/($lang)._index.tsx
+++ app/routes/($lang)._index.tsx
@@ -10,63 +10,19 @@ import { ContentPage } from "~/features/landing-page/page";
 
 export const middleware: MiddlewareFunction[] = [pageMiddleware];
 
-export async function loader({ context, request }: Route.LoaderArgs) {
+export async function loader({ context }: Route.LoaderArgs) {
   const pageContent = context.get(pageMiddlewareContext);
-
   return pageContent;
 }
 
 export function meta({ loaderData }: Route.MetaArgs) {
-  if (
-    !("title" in loaderData) ||
-    !("description" in loaderData) ||
-    !loaderData.title ||
-    !loaderData.description
-  ) {
-    return [
-      {
-        title:
-          "NARA Website Starter Kit — Modern, Flexible, Type-Safe Boilerplate",
-      },
-      {
-        name: "description",
-        content:
-          "A fast, opinionated starter template for building full-stack React apps powered by React Router v7, Cloudflare Workers, and modern tooling. Built with a focus on type safety, performance, and developer ergonomics.",
-      },
-    ];
-  }
-
-  return [
-    { title: loaderData.title },
-    { name: "description", content: loaderData.description },
-  ];
+  const { title, description } = loaderData;
+  return [{ title }, { name: "description", content: description }];
 }
 
 export default function Page({ loaderData }: Route.ComponentProps) {
-  if (!loaderData || "error" in loaderData) return null;
-
-  const {
-    title,
-    description,
-    githubRepository,
-    commercialLink,
-    showcases,
-    steps,
-    featuresConfig,
-  } = loaderData;
-
   return (
-    <PageContext.Provider
-      value={{
-        title,
-        description,
-        githubRepository,
-        commercialLink,
-        showcases,
-        steps,
-        featuresConfig,
-      }}
-    >
+    <PageContext.Provider value={loaderData}>
       <ContentPage />
     </PageContext.Provider>
   );
