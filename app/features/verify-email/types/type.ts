e74b332391dc04d71f59c27951d11ba7c25985c4
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2025-09-17 16:19:05 +0700

    feat: 🎸 apply middleware for verify email page

--- app/features/verify-email/types/type.ts
+++ app/features/verify-email/types/type.ts
@@ -1,5 +1,13 @@
+import type { SupportedLanguage } from "~/lib/i18n";
+
 export type VerifyEmailPageProps = {
   isSuccess?: boolean;
   error?: string | null;
   message?: string | null;
+};
+
+export type PageInformation = {
+  title?: string;
+  description?: string;
+  language: SupportedLanguage;
 };
