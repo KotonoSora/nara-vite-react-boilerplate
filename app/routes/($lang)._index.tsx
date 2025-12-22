0b1dcdb7756ab42a563a688b9791201222b6cd9c
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2025-12-22 16:56:34 +0700

    feat: 🎸 implement meta data for SEO

--- app/routes/($lang)._index.tsx
+++ app/routes/($lang)._index.tsx
@@ -20,17 +20,51 @@ export async function loader({ context }: Route.LoaderArgs) {
 
 export function meta({ loaderData }: Route.MetaArgs) {
   const { title, description } = loaderData;
+
+  const canonicalUrl =
+    import.meta.env.VITE_PROD_DOMAIN || import.meta.env.VITE_DEV_DOMAIN;
+  const siteName = "KotonoSora";
+  const locale = "en_US";
+  const language = "en-US";
+  const socialImage = canonicalUrl + "/assets/png/social-media.png";
+  const fallbackSocialImage = SocialPreview;
+  const imageContent = socialImage ?? fallbackSocialImage;
+  const twitterHandle = "@kotonosora";
+  const modifiedTime = "2025-01-01T00:00:00+00:00";
+  const publisherUrl = "@kotonosora";
+
   return [
+    { charSet: "utf-8" },
     { title },
     { name: "description", content: description },
-    // Meta tags for SEO and Social Media
+    { name: "language", content: language },
+    { tagName: "link", rel: "canonical", href: canonicalUrl },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
+      name: "robots",
+      content:
+        "index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+    },
+    { property: "og:locale", content: locale },
+    { property: "og:type", content: "website" },
     { property: "og:title", content: title },
     { property: "og:description", content: description },
+    { property: "og:url", content: canonicalUrl },
+    { property: "og:site_name", content: siteName },
+    {
+      property: "article:publisher",
+      content: publisherUrl,
+    },
+    { property: "article:modified_time", content: modifiedTime },
+    { property: "og:image", content: imageContent },
+    { property: "og:image:width", content: "1280" },
+    { property: "og:image:height", content: "640" },
+    { property: "og:image:type", content: "image/png" },
+    { name: "twitter:card", content: "summary_large_image" },
     { name: "twitter:title", content: title },
     { name: "twitter:description", content: description },
-    { property: "og:image", content: SocialPreview },
-    { name: "twitter:image", content: SocialPreview },
-    { property: "og:type", content: "website" },
+    { name: "twitter:site", content: twitterHandle },
+    { name: "twitter:image", content: imageContent },
     {
       name: "keywords",
       content:
