4f5b912201b227f99dcccc67eafe630089c5cff1
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2026-05-29 14:03:23 +0700

    feat: 🎸 config wrangler and workflows

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    databaseId: "your-database-id",
+    databaseId: process.env.D1_DATABASE_ID ?? "",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_API_TOKEN!,
   },
