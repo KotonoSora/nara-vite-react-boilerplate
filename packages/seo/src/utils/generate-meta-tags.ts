71c5c62f8708e8cf332d25426bcbbb9b69a4219d
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2026-05-30 08:44:24 +0700

    feat: 🎸 change config tsconfig in packages

--- packages/seo/src/utils/generate-meta-tags.ts
+++ packages/seo/src/utils/generate-meta-tags.ts
@@ -1,10 +1,7 @@
 import { getIntlLocaleByLanguage } from "@kotonosora/i18n";
 import { DEFAULT_LANGUAGE } from "@kotonosora/i18n-locales";
 
-import type {
-  GenerateMetaTagsParams,
-  GenerateMetaTagsResponse,
-} from "../types";
+import type { GenerateMetaTagsParams, GenerateMetaTagsResponse } from "@/types";
 
 import {
   author,
@@ -13,7 +10,7 @@ import {
   publisherUrl,
   siteName,
   twitterHandle,
-} from "../constants/common";
+} from "@/constants/common";
 
 /**
  * Generates SEO meta tags for a page.
