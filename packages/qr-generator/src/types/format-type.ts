298ea1824eb1dda1486d595a3dbdda19ec62b9f2
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2026-05-30 11:45:02 +0700

    feat: 🎸 remove folder qr generator

--- packages/qr-generator/src/types/format-type.ts
+++
@@ -1,7 +0,0 @@
-export type QRCodeFormat = "png" | "jpg";
-
-export type QRCodeOptions = {
-  size: number;
-  level: "L" | "M" | "Q" | "H";
-  marginSize: number;
-};
--- packages/qr-generator/src/types/preview-section-type.ts
+++
@@ -1,9 +0,0 @@
-import { type RefObject } from "react";
-
-import { QRCodeOptions } from "./format-type";
-
-export type QRPreviewSectionProps = {
-  qrRef: RefObject<HTMLDivElement | null>;
-  text: string;
-  options: QRCodeOptions;
-};
