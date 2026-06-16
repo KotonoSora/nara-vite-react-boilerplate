b4519176375ba33cde6f726aaa238754c4542721
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2026-06-16 16:25:23 +0700

    feat: 🎸 add button scroll to top for blog layout

--- app/features/blog/page.tsx
+++ app/features/blog/page.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router";
 
+import { ButtonScrollToTop } from "~/features/shared/components/button-scroll-to-top";
 import { FooterSection } from "~/features/shared/components/footer-section";
 import { HeaderNavigation } from "~/features/shared/header-navigation";
 
 export function BlogPage() {
   return (
-    <main className="min-h-screen bg-background content-visibility-auto flex flex-col">
+    <>
       <HeaderNavigation />
       <Outlet />
       <FooterSection />
-    </main>
+      <ButtonScrollToTop />
+    </>
   );
 }
